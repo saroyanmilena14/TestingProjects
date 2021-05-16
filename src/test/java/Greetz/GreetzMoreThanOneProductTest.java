fb2f13131ccf2c83aea2347a95d06b51b123c390
unknown <milenasaroyan14@gmail.com>
2021-05-16 20:02:18 +0400

    changes

--- src/test/java/Greetz/GreetzMoreThanOneProductTest.java
+++ src/test/java/Greetz/GreetzMoreThanOneProductTest.java
@@ -13,36 +13,57 @@ import java.util.List;
 import java.util.Random;
 
 public class GreetzMoreThanOneProductTest {
-
-    @Test
-    public void CheckingPriceChangesTest() {
-        System.setProperty("webdriver.chrome.driver", "src\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.greetz.nl/ballonnen");
-        WebDriverWait wait = new WebDriverWait(driver, 25);
-
-        List<WebElement> allBallonnen = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//div[@class='clearfix' and  @infinite-scroll='main.loadNextPage()']//div[@class='b-products-grid__item' and @ng-class]"), 20));
-        WebElement randomBallonnen = allBallonnen.get(new Random().nextInt(allBallonnen.size()));
-        randomBallonnen.click();
-        WebElement priceBefore = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-qa-ref=normal-price]")));
-        String priceBeforeText = priceBefore.getText().substring(1).replace(",", ".");
-        Double priceBeforeValue = Double.parseDouble(priceBeforeText);
-        WebElement inputAmount = driver.findElement(By.xpath("//div[@class='page-detail__select']//input[@data-qa-ref='product-amount']"));
-        String counttext = "4";
-
-        Double count = Double.parseDouble(counttext);
-        Double expectedPrice = count * priceBeforeValue;
-        String expectedPriceText = String.valueOf(expectedPrice).replace(".", ",");
-        inputAmount.sendKeys(Keys.BACK_SPACE, counttext);
+    private WebDriver driver;
+    private static WebDriverWait wait;
+    Random rand = new Random();
 
 
-        WebElement priceAfter = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), '" + expectedPriceText + "')]")));
-        String priceAfterText = priceAfter.getText().substring(1).replace(",", ".");
-        Double priceAfterValue = Double.parseDouble(priceAfterText);
-        driver.quit();
-        Assert.assertEquals(expectedPrice, priceAfterValue, "The expected price " + expectedPrice + " doesn't equal to " + priceAfterValue);
+        @Test
+        public void CheckingPriceChangesTest() throws InterruptedException {
 
+            System.setProperty("webdriver.chrome.driver",
+                    "src\\main\\resources\\chromedriver.exe");
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, 30);
+            driver.manage().window().maximize();
+            driver.get("https://www.greetz.nl/ballonnen ");
+            By acceptCookiesElemLoc = By.cssSelector(".b-buttonbar--button.b-button.cookieBar-old-style__button");
+            WebElement acceptCookieElem = wait.until(ExpectedConditions.elementToBeClickable(acceptCookiesElemLoc));
+            acceptCookieElem.click();
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(acceptCookiesElemLoc));
+            By productsElems = By.xpath("//div[@class='b-products-grid__item']//*[@data-qa-ref='product-wall-item']");
+            List<WebElement> productsElements = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(productsElems, 15));
+            WebElement elementRandom = getRandomElement(productsElements);
+            WebElement element = wait.until(ExpectedConditions.visibilityOf(elementRandom));
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView(true);", element);
+            element.click();
+            By prodAmount = By.cssSelector("[data-qa-ref=product-amount]");
+            WebElement productAmountElement = wait.until(ExpectedConditions.visibilityOfElementLocated(prodAmount));
+            By priceNormal = By.cssSelector("[data-qa-ref=normal-price]");
+            WebElement productPriceElement = driver.findElement(priceNormal);
+            String prodPrice = productPriceElement.getText();
+            String prodPriceForDouble = prodPrice.replace(",", ".").replaceAll("[^0-9.]", "");
+            double prodNumberPrice = Double.valueOf(prodPriceForDouble);
+            productAmountElement.clear();
+            String randomA = String.valueOf(getRandomNumber());
+            productAmountElement.sendKeys(randomA);
+            String valueInput = productAmountElement.getAttribute("value");
+            wait.until(ExpectedConditions.invisibilityOfElementWithText(priceNormal, prodPrice));
+            String prodPriceUpdated = productPriceElement.getText().replace(",", ".").replaceAll("[^0-9.]", "");
+            double prodNumberPriceUpdated = Double.valueOf(prodPriceUpdated);
+            double totalAmount = (Double.valueOf(valueInput) * prodNumberPrice) * 100 / 100.0;
+            Assert.assertEquals(totalAmount, prodNumberPriceUpdated, "The total amount " + totalAmount + " for quantity: " + randomA + " does not match with total amount of " + prodNumberPriceUpdated + " presented on the site.");
+            driver.quit();
+        }
+        public WebElement getRandomElement(List<WebElement> list) {
+            return list.get(rand.nextInt(list.size()));
+        }
+        public int getRandomNumber() {
+            return rand.nextInt(4) + 2;
+        }
 
     }
-}
+
+
+
